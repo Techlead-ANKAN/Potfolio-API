f604a8b31a153d1ba5e3a438372e02bfe01e03a1
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-06-24 23:44:23 +0530

    refactor: simplify data file path handling in portfolio API

--- api/portfolio.js
+++ api/portfolio.js
@@ -1,8 +1,12 @@
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
 export default async function handler(req, res) {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const dataPath = path.join(__dirname, "../src/data.json");
   const data = JSON.parse(
-    await fs.readFile(new URL("../../src/data.json", import.meta.url), "utf-8")
+    await fs.readFile(dataPath, "utf-8")
   );
   res.status(200).json(data);
 }
