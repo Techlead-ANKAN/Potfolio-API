40ed3a5681cb0e909196bd26b26bc88ffadd9d9c
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-06-24 23:28:00 +0530

    feat: add dotenv for environment variable management and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // index.js
 import express from "express";
 import fs from "fs/promises";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 // Read JSON data asynchronously
